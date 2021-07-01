f5b00a98b6e3bce7bab777f68c5be199efffc9e2
Slin-Miraka <76611803+Slin-Miraka@users.noreply.github.com>
2021-07-02 00:48:15 +0800

    Create Time_series.py

--- Time_series.py
+++ Time_series.py
@@ -131,6 +131,9 @@ fig, ax = plt.subplots(figsize=(9.5,6))
 sm.qqplot(R, line ='45',ax = ax)
 right_col.pyplot(plt)
 
+st.markdown("✅ Return's ACF. vs. Squred Return's ACF")
+call = st.checkbox("Return's ACF. vs. Return's PACF")
+
 fig = autocorrelation_plot(R, lags = 50, figsize=(20,4),Title = "return", simbol = symbol)
 st.pyplot(fig)
 
