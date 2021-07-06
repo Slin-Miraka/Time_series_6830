906db40e57a09fc31d9180e2c3b58b62be6048a4
Slin-Miraka <76611803+Slin-Miraka@users.noreply.github.com>
2021-07-06 11:33:14 +0800

    Update Time_series.py

--- Time_series.py
+++ Time_series.py
@@ -180,6 +180,7 @@ if ljbox_test:
     row3_1.write("Ljung–Box test for the {}'s return series".format(symbol))
     R_test = sm.stats.acorr_ljungbox(R, lags=ljboxlags, return_df=True)
     row3_1.write(R_test)
+    row3_2.write("")
     row3_2.write("Ljung–Box test for the {}'s squared return series".format(symbol))
     R2_test =sm.stats.acorr_ljungbox(R**2, lags=ljboxlags, return_df=True)
     row3_2.write(R2_test)
