27b3c1576ca64727803ee347e0653c1de259fb53
Slin-Miraka <76611803+Slin-Miraka@users.noreply.github.com>
2021-07-06 12:03:33 +0800

    Update Time_series.py

--- Time_series.py
+++ Time_series.py
@@ -205,12 +205,12 @@ if ljbox_test:
 
 if ARCH_test:
     mean_c_return = R - np.mean(R)
-    row3_2.write("")
-    row3_2.write("")
-    ARCHlags = row3_2.slider('Slide me to choose the lags for ARCH test', min_value=5, max_value=50, step = 1, value = 20)
-    row3_1.write("**ARCH test for the {}'s return series**".format(symbol))
-    ARCH = ARCH_test(mean_c_return, lags=ARCHlags)
-    row3_1.write(ARCH)
+    row3_2.write(mean_c_return)
+    #row3_2.write("")
+    #ARCHlags = row3_2.slider('Slide me to choose the lags for ARCH test', min_value=5, max_value=50, step = 1, value = 20)
+    #row3_1.write("**ARCH test for the {}'s return series**".format(symbol))
+    #ARCH = ARCH_test(mean_c_return, lags=ARCHlags)
+    #row3_1.write(ARCH)
 
 
 
